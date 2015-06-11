c648b08cc7398fe501a2b3bb53bb46ea6e78e837
Qendrim <QGjevukaj@gmail.com>
2015-06-11 12:08:38 -0400

    Models.py and Admin.py modified Improved on Database Schema

--- Capstone/menu/admin.py
+++ Capstone/menu/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from menu.models import Menu, FoodItem, FoodType, Review  # , organizedMenu
+from menu.models import Menu, FoodItem, FoodType, Review,Rating  # , organizedMenu
 
 # Register Models are will be edited in the Admin screen
 # This may not include all models
 
 class FoodTypeAdmin(admin.ModelAdmin):
     # list_display = ('FType')
-    ordering = ['dish_type']
+    ordering = ['food_type']
 
 class FoodAdmin(admin.StackedInline):
       model = FoodItem
@@ -17,20 +17,16 @@ class Reviews(admin.StackedInline):
      extra = 0
 
 class MenuAdmin(admin.ModelAdmin):
+    ordering = ['menu_title']
     fieldsets = [
         (None, {'fields':['menu_title']}),
     ]
     inlines = [FoodAdmin,Reviews]
 
-class ReviewAdmin(admin.ModelAdmin):
-     #fieldsets = [
-     #    (None, {'fields': ['m_title']}),
-     #    (None, {'fields': ['d_type']})
-     #          # {'fields': ['dish_type']}
-    # ]
-   #inlines = [Reviews]
-   ordering = ['reviews']
+class RatingAdmin(admin.ModelAdmin):
+     ordering = ['rating']
 
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(FoodType, FoodTypeAdmin)
+admin.site.register(Rating,RatingAdmin)
 
--- Capstone/menu/models.py
+++ Capstone/menu/models.py
@@ -8,30 +8,51 @@ class Menu(models.Model):
     def __str__(self): #This returns the value of title in the admin view
         return self.menu_title
 
+    class Meta: #defines menu admin default name
+        verbose_name = 'Menu Management'
+        verbose_name_plural = 'Menu Management'
+
 #This model is utilized to organize different dish types on a menu
 class FoodType(models.Model):
-    dish_type = models.CharField(max_length=10,default=None)
+    food_type = models.CharField(max_length=10,default=None)
     def __str__(self):
-        return self.dish_type
+        return self.food_type
+
+    class Meta:
+        verbose_name = 'Category for Food Type'
+        verbose_name_plural = 'Category for Food Type'
 #This model is setup with a many items to one menu and many items to one type organization
 class FoodItem(models.Model):
+    #One To Many Relations
     menu_title = models.ForeignKey(Menu, default=None) #requires a specific menu
-    dish_type = models.ForeignKey(FoodType,default=None)#requires a specific type of food item
-    dish_name = models.CharField(max_length=200) #then define the dishname to assign to this menu/type
+    food_type = models.ForeignKey(FoodType,default=None)#requires a specific type of food item
+    #Data
+    food_name = models.CharField(max_length=200) #then define the dishname to assign to this menu/type
     def __str__(self):
-        return self.dish_name
-#This model will display a chosen menu and the contents
-#Add rating module 1-5 like footype
+        return self.food_name
+
+#This Model is utilized to generate a variable rating system e.g. 1-5, 1-10
+#This data is defined within the django admin
+class Rating(models.Model):
+    rating = models.CharField(max_length=1)
+    def __str__(self):
+        return self.rating
+
+    class Meta:
+        verbose_name = 'Rating System'
+        verbose_name_plural = 'Rating System'
+
 class Review(models.Model):
     #Keep in mind the many to one relationship in these cases imply
     #that you could access reviews as a whole or use filters based on
     #the variables below e.g. Menu Title->All Reviews or more Specific
     #would be Menu Title->Dish Name->All reviews
     menu_title = models.ForeignKey(Menu, default=None) #One menu points to many reviews
-    dish_type = models.ForeignKey(FoodType, default=None)#One dish_type points to many reviews
-    dish_name = models.ForeignKey(FoodItem,default=None)#One dish points to many reviews
+    food_type = models.ForeignKey(FoodType, default=None)#One dish_type points to many reviews
+    food_name = models.ForeignKey(FoodItem,default=None)#One dish points to many reviews
+    rating = models.ForeignKey(Rating,default=None)
     review_date = models.DateField('Published on')
-    #Menu.objects.all().filter(title=m_id)
-    reviews = models.CharField(max_length=200, default=None)
+
+    review = models.TextField(max_length=200, default=None)
     def __str__(self):
         return self.reviews
