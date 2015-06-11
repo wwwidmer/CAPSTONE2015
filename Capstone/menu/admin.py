953b853d6435ac8eea67d13fe0440962ccdb5d16
Qendrim <QGjevukaj@gmail.com>
2015-06-11 09:51:30 -0400

    Models.py and Admin.py modified

--- Capstone/menu/admin.py
+++ Capstone/menu/admin.py
@@ -1,14 +1,36 @@
 from django.contrib import admin
-from menu.models import Menu, FoodItem
+from menu.models import Menu, FoodItem, FoodType, Review  # , organizedMenu
 
 # Register Models are will be edited in the Admin screen
 # This may not include all models
 
-class MenuAdmin(admin.ModelAdmin):
-    ordering = ["-id"]
-class FoodAdmin(admin.ModelAdmin):
-    list_display = ('dishname',id)
-    ordering = ("-id",'dishname')
+class FoodTypeAdmin(admin.ModelAdmin):
+    # list_display = ('FType')
+    ordering = ['dish_type']
 
-admin.site.register(Menu,MenuAdmin)
-admin.site.register(FoodItem,FoodAdmin)
+class FoodAdmin(admin.StackedInline):
+      model = FoodItem
+      extra = 0
+
+class Reviews(admin.StackedInline):
+     model = Review
+     extra = 0
+
+class MenuAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['menu_title']}),
+    ]
+    inlines = [FoodAdmin,Reviews]
+
+class ReviewAdmin(admin.ModelAdmin):
+     #fieldsets = [
+     #    (None, {'fields': ['m_title']}),
+     #    (None, {'fields': ['d_type']})
+     #          # {'fields': ['dish_type']}
+    # ]
+   #inlines = [Reviews]
+   ordering = ['reviews']
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(FoodType, FoodTypeAdmin)
+
--- Capstone/menu/models.py
+++ Capstone/menu/models.py
@@ -2,23 +2,36 @@ from django.db import models
 
 # Create your models here. See Issue on github for database schema
 # class nameOfModel(models.Model): ....
-
-
+#This model is utilized to setup a new menu name (eg restaurant name)
 class Menu(models.Model):
-    title = models.CharField(max_length=200)
+    menu_title = models.CharField(max_length=200)
+    def __str__(self): #This returns the value of title in the admin view
+        return self.menu_title
 
+#This model is utilized to organize different dish types on a menu
+class FoodType(models.Model):
+    dish_type = models.CharField(max_length=10,default=None)
+    def __str__(self):
+        return self.dish_type
+#This model is setup with a many items to one menu and many items to one type organization
 class FoodItem(models.Model):
-    type = ""
-    dishname = models.CharField(max_length=200)
-    menu = models.ForeignKey(Menu, default=None)
-
-    def getAverage(self):
-        return 1
-    def getSimilar(self):
-        return 1
-
+    menu_title = models.ForeignKey(Menu, default=None) #requires a specific menu
+    dish_type = models.ForeignKey(FoodType,default=None)#requires a specific type of food item
+    dish_name = models.CharField(max_length=200) #then define the dishname to assign to this menu/type
+    def __str__(self):
+        return self.dish_name
+#This model will display a chosen menu and the contents
+#Add rating module 1-5 like footype
 class Review(models.Model):
-    #reviewer = models.CharField(max_length=200)
-    food = models.ForeignKey(FoodItem, default=None)
-    rating = models.IntegerField(default=0)
-    comment = models.CharField(max_length=500)
+    #Keep in mind the many to one relationship in these cases imply
+    #that you could access reviews as a whole or use filters based on
+    #the variables below e.g. Menu Title->All Reviews or more Specific
+    #would be Menu Title->Dish Name->All reviews
+    menu_title = models.ForeignKey(Menu, default=None) #One menu points to many reviews
+    dish_type = models.ForeignKey(FoodType, default=None)#One dish_type points to many reviews
+    dish_name = models.ForeignKey(FoodItem,default=None)#One dish points to many reviews
+    review_date = models.DateField('Published on')
+    #Menu.objects.all().filter(title=m_id)
+    reviews = models.CharField(max_length=200, default=None)
+    def __str__(self):
+        return self.reviews
