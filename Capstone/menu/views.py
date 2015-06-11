6dfee7fa0873cb0188e0eb34c12ee61ddfae823c
william <williamwidmerr@gmail.com>
2015-06-11 18:19:19 -0400

    Edited out HEAD errors

--- Capstone/menu/views.py
+++ Capstone/menu/views.py
@@ -9,7 +9,6 @@ def index(request):
     return render_to_response("index.html",context)
 
 def render_menu(request,m_id):
-<<<<<<< HEAD
     menu = Menu.objects.all().filter(id=m_id)
     food = FoodItem.objects.all().filter(menu__id=m_id)
     context = {'menu':menu, 'food':food}
@@ -17,7 +16,6 @@ def render_menu(request,m_id):
 
 def render_food(request,f_id):
     food = FoodItem.objects.all().filter(id=f_id)
-=======
     # get menu by id, get all food associated with
     menu = Menu.objects.all().filter(menu_title=m_id)
     food = FoodItem.objects.all().filter(menu=menu) #accessing menu within FoodItem and limiting food t
@@ -28,7 +26,6 @@ def render_food(request,f_id):
 def render_food(request,f_id):
     #get food by id, get all comments associated with
     food = FoodItem.objects.all().filter(f_id=f_id)
->>>>>>> 408117ef9c6781687a555f55c0f45d1b518710cc
     reviews = Review.objects.all().filter(food__id=f_id)
     #context = {'food':food}
     return render_to_response("food.html",context)
@@ -52,15 +49,12 @@ def get_Average(food):
 
 #  All below isn't working - don't worry for now...
 def render_search(request):
-<<<<<<< HEAD
     query_string = ''
     menu = ''
     food = ''
-=======
     #query_string = ''
     #menu = ""
     #food = ""
->>>>>>> 408117ef9c6781687a555f55c0f45d1b518710cc
     found = None
     if('search' in request.GET) and request.GET['search'].strip():
         query_string = request.GET.get('search')
