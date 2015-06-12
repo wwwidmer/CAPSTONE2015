c5494168dc956640a30eac5aea96573848cf392c
william <williamwidmerr@gmail.com>
2015-06-12 15:07:46 -0400

    Minor edits, added new image assets, minor style changes and formatting.
    food.html now has an average Star rating image.

--- Capstone/menu/forms.py
+++ Capstone/menu/forms.py
@@ -4,6 +4,7 @@ from menu.models import Review, FoodItem
 from django import forms
 
 class ReviewForm(forms.ModelForm):
+    rating = forms.IntegerField(min_value=0,max_value=5)
     class Meta:
         model = Review
         fields = ['rating','review']
--- Capstone/menu/tests.py
+++ Capstone/menu/tests.py
@@ -18,7 +18,21 @@ class FoodTestCase(TestCase):
         food2 = FoodItem.objects.create()
 
 class ReviewTestcase(TestCase):
-    pass
+    def setUp(self):
+        menu1 = Menu.objects.create(title="T1")
+        type1 = FoodType.objects.create()
+        food1 = FoodItem.objects.create()
+        rev1 = Review.objects.create()
+        rev2 = Review.objects.create()
+
 
 class SearchTestCase(TestCase):
-    pass
+    def setUp(self):
+        menu1 = Menu.objects.create(title="T1")
+        menu2 = Menu.objects.create(title="T1")
+        menu3 = Menu.objects.create(title="T1")
+        menu4 = Menu.objects.create(title="T1")
+        food1 = FoodItem.objects.create()
+        food2 = FoodItem.objects.create()
+        food3 = FoodItem.objects.create()
+        food4 = FoodItem.objects.create()
