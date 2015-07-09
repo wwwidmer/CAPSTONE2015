930b62064c612cf6ed2e688e2bca8f4d4a834ac6
william <williamwidmerr@gmail.com>
2015-07-08 21:53:16 -0400

    General cleanup, re commenting, edited some models to make testing possible

--- Capstone/menu/models.py
+++ Capstone/menu/models.py
@@ -4,101 +4,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from math import floor
 from django.core import signals
 
-
 '''
-Main Database tables
-AbstractMenuItem - All tables will inherit from this - defines basic non redundant information
-***Inheritance should only apply to common information
-Menu - Table of inventory information (Where the food is)
-FoodType - Table of types (used for grouping FoodItems) * ManyToMany relationship, no common redundant information
-FoodItem - Table of Stock (Food) Information * FoodItem/MenuName share a 'name'space but review does not
-Review - Saved Information about Stock * Reviewcomment is a text field, this should not be passed to each classes
-table in our database
+Helper Methods
 '''
-#Image resizing function allowing each class inheriting abstract MenuItem to define its own parameters during runtime
-def resizeLogo(instance, self, x, y):
-    if not self.logo:
-        self.logo='default.png'
-    super(instance, self).save()
-    logo = Image.open(self.logo)
-    size = (x, y)
-    logo = logo.resize(size, Image.ANTIALIAS)
-    logo.save(self.logo.path)
-
-#Define upload path during runtime for each class inheriting abstractMenuItem
-def uploadPath(instance, filename):
-    return ''.join([instance.uploadPath, filename])
-
-
-'''
-#abstract multiple table inheritance, models inherit these variables.
-#This is data that is shared and joined to each class that inherits this abstract class
-#Only one table is created in our database for this abstract class and the one that inherits it.
-'''
-class abstractMenuItem(models.Model):
-    createdOn = models.DateField('Published On', null=True, blank=True) #Disables Requirement of CreatedOn
-    createdBy = models.CharField(max_length=30,default='', null=True, blank=True)#Remove blank/null for required
-    logo = models.ImageField(upload_to=uploadPath, blank=True, editable=True, verbose_name='logo')
-    rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    isActive = models.BooleanField(default=True)
-
-    class Meta: #Abstract model, models that inherit this abstractClass do not share data on the same table
-        abstract = True
-
-
-class Menu(abstractMenuItem):
-    menuName = models.CharField(max_length=30, default='')
-    gid = models.IntegerField(default=0)
-    uploadPath = 'menuLogo/'
-
-    def save(self):
-        resizeLogo(Menu, self, 50, 50)
-        set_menu_isActive(self.id,self.isActive)
-
-
-    def __str__(self):
-        return self.menuName
-
-    class Meta:
-        verbose_name = 'Menu Management'
-        verbose_name_plural = 'Menu Management'
-
-#This is its own table of types
-class FoodType(models.Model): #Food Types don't inherit anything, contains single attribute with many to many rel.
-    type = models.CharField(max_length=30, default='')
-
-    def __str__(self):
-        return self.type
-
-    class Meta:
-        verbose_name = 'Category for Food Type'
-        verbose_name_plural = 'Category for Food Type'
-
-#This table continues off of the menu table inheriting its variables
-class FoodItem(abstractMenuItem):
-    menuName = models.ForeignKey(Menu, default=None)
-    dishName = models.CharField(max_length=30,default='')
-    type = models.ManyToManyField(FoodType, default=None)
-    uploadPath = 'foodLogo/'
-
-    def save(self):
-        resizeLogo(FoodItem, self, 50, 50)
-        set_food_isActive(self.id,self.isActive)
-
-    def __str__(self):
-        return self.dishName
-
-class Review(abstractMenuItem):
-    foodItemName = models.ForeignKey(FoodItem, default=None)
-    reviewComment = models.TextField(max_length=200, default=None)
-    uploadPath = 'reviewLogo/'
-
-    def save(self):
-        resizeLogo(Review, self, 50, 50)
-
-    def __str__(self):
-        return self.reviewComment
-
 # Get average by ID, takes either food or menu ID passed as parameters (None if not searching)
 def get_Average(food_id, menu_id):
     try:
@@ -114,7 +22,6 @@ def get_Average(food_id, menu_id):
     except ZeroDivisionError:
         return 0
 
-
 # Get all items 'below' a certain model and set their state accordingly.
 def set_menu_isActive(menu_id, state):
     food = FoodItem.objects.all().filter(menuName=menu_id)
@@ -128,3 +35,77 @@ def set_food_isActive(food_id,state):
     for item in list(reviews):
         item.isActive = state;
         item.save()
+
+#Image resizing function allowing each class inheriting abstract MenuItem to define its own parameters during runtime
+def resizeLogo(instance, self, x, y):
+    if not self.logo:
+        self.logo='default.png'
+    super(instance, self).save()
+    logo = Image.open(self.logo)
+    size = (x, y)
+    logo = logo.resize(size, Image.ANTIALIAS)
+    logo.save(self.logo.path)
+
+#Define upload path during runtime for each class inheriting abstractMenuItem
+def uploadPath(instance, filename):
+    return ''.join([instance.uploadPath, filename])
+
+'''
+Main Database tables
+AbstractMenuItem - All tables will inherit from this - defines basic non redundant information
+Menu - Table of inventory information (Where the food is)
+FoodType - Table of types (used for grouping FoodItems) * ManyToMany relationship, no common information needed
+FoodItem - Table of Stock (Food) Information
+Review - Saved Information about Stock
+table in our database
+'''
+
+class FoodType(models.Model):
+    type = models.CharField(max_length=30, default='')
+    def __str__(self):
+        return self.type
+    class Meta:
+        verbose_name = 'Category for Food Type'
+        verbose_name_plural = 'Category for Food Type'
+
+class abstractMenuItem(models.Model):
+    createdOn = models.DateField('Published On', null=True, blank=True)
+    createdBy = models.CharField(max_length=30,default='', null=True, blank=True)
+    logo = models.ImageField(upload_to=uploadPath, blank=True, editable=True, verbose_name='logo')
+    rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    isActive = models.BooleanField(default=True)
+    class Meta: #Abstract model, models that inherit this abstractClass do not share data on the same table
+        abstract = True
+
+class Menu(abstractMenuItem):
+    menuName = models.CharField(max_length=30, default='')
+    gid = models.IntegerField(default=0,blank=True)
+    uploadPath = 'menuLogo/'
+    def save(self,force_insert=False,using=None):
+        resizeLogo(Menu, self, 50, 50)
+        set_menu_isActive(self.id,self.isActive)
+    def __str__(self):
+        return self.menuName
+    class Meta:
+        verbose_name = 'Menu Management'
+        verbose_name_plural = 'Menu Management'
+
+class FoodItem(abstractMenuItem):
+    menuName = models.ForeignKey(Menu, default=None)
+    dishName = models.CharField(max_length=30,default='')
+    type = models.ManyToManyField(FoodType, default=None)
+    uploadPath = 'foodLogo/'
+    def save(self,force_insert=False,using=None):
+        resizeLogo(FoodItem, self, 50, 50)
+        set_food_isActive(self.id,self.isActive)
+    def __str__(self):
+        return self.dishName
+
+class Review(abstractMenuItem):
+    foodItemName = models.ForeignKey(FoodItem, default=None)
+    reviewComment = models.TextField(max_length=200, default=None)
+    uploadPath = 'reviewLogo/'
+    def save(self,force_insert=False,using=None):
+        resizeLogo(Review, self, 50, 50)
+    def __str__(self):
+        return self.reviewComment
