e81cc0e14da31807055278ef6c97285ade99b4d3
Richard <richard77927@hotmail.com>
2015-07-23 15:42:42 -0400

    .

---
+++ Capstone/Capstone/urls.py
@@ -0,0 +1,41 @@
+"""Capstone URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+from menu import views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'menu.views.index'),
+    url(r'^ajax/food/','menu.views.ajax_get_food_by_id'),
+    url(r'^ajax/menu/','menu.views.ajax_get_menu_by_id'),
+    url(r'^ajax/menu/food/','menu.views.ajax_get_food_by_menu_id'),
+    url(r'^ajax/review/','menu.views.ajax_get_review_by_food'),
+    url(r'^menus/food/(?P<f_id>\d+)', 'menu.views.render_food'),
+    url(r'^menus/food/type/(?P<t_id>\d+)', 'menu.views.render_browse_type_food'),
+    url(r'^menus/gid/(?P<g_id>[^/]+)/(?P<name>[\w\ ]+)', 'menu.views.render_menu_by_gid'),
+    url(r'^menus/gid/(?P<g_id>[^/]+)', 'menu.views.render_menu_by_gid'),
+    url(r'^menus/type/(?P<t_id>\d+)', 'menu.views.render_browse_type_food'), 
+    url(r'^menus/type/', 'menu.views.render_browse_type_index'),
+    url(r'^menus/(?P<m_id>[^/]+)', 'menu.views.render_menu'),
+    url(r'^menus', 'menu.views.render_browse_top_menu'),
+    url(r'^search/results', 'menu.views.render_search'),
+    url(r'^search/', 'menu.views.render_search_index'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+]
---
+++ Capstone/static/assets/radar search.js
@@ -0,0 +1,75 @@
+var map;
+var infoWindow;
+var service;
+
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: new google.maps.LatLng(-33.8668283734, 151.2064891821),
+    zoom: 15,
+    styles: [
+      {
+        stylers: [
+          { visibility: 'simplified' }
+        ]
+      },
+      {
+        elementType: 'labels',
+        stylers: [
+          { visibility: 'off' }
+        ]
+      }
+    ]
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
+
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'best view'
+  };
+  service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    createMarker(result);
+  }
+}
+
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: {
+      // Star
+      path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      scale: 1/4,
+      strokeColor: '#bd8d2c',
+      strokeWeight: 1
+    }
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    service.getDetails(place, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
