ab9d0b5563b525ce41ef9a036012d6dbe4832b58
Richard <richard77927@hotmail.com>
2015-07-23 17:15:08 -0400

    Image enlarge done.

--- Capstone/static/assets/image.js
+++ Capstone/static/assets/image.js
@@ -1,22 +1,31 @@
-
-/* FancyBox - image enlarge */
-
+/*
+ FancyBox - image enlarge
+ */
 $(document).ready(function() {
 	$(".fancybox").fancybox();
-});
 
-$(document).ready(function() {
+	/* item simple */
+	$("#single_1").fancybox({
+		helpers : {
+			title : {
+				type : 'float'
+			}
+		}
+	});
+	
+	/* item review*/
 	$(".fancybox-thumb").fancybox({
-		prevEffect	: 'none',
-		nextEffect	: 'none',
-		helpers	: {
-			title	: {
-				type: 'outside'
+		prevEffect : 'none',
+		nextEffect : 'none',
+		helpers : {
+			title : {
+				type : 'outside'
 			},
-			thumbs	: {
-				width	: 50,
-				height	: 50
+			thumbs : {
+				width : 50,
+				height : 50
 			}
 		}
 	});
 });
+
